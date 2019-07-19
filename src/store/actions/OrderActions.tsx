724a8d378d97dc48af2712e91e6f96c83eebcc1a
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-19 00:21:15 -0300

    add order list to order store

--- src/store/actions/OrderActions.tsx
+++ src/store/actions/OrderActions.tsx
@@ -1,7 +1,7 @@
-
 import * as config from '@src/core/config';
-import { POSTORDERSTARTED, POSTORDERENDED } from './ActionTypes';
-export const postOrder = (order) => {
+import { LISTORDERSSTARTED, LISTORDERSENDED, POSTORDERSTARTED, POSTORDERENDED, STOREORDERS } from './ActionTypes';
+import { OrderModel, ArtistModel } from '@favid-inc/api';
+export const postOrder = order => {
   return async dispatch => {
     dispatch(postOrderStarted());
     await fetch(`${config.firebase.databaseURL}/order.json`, {
@@ -17,6 +17,44 @@ export const postOrder = (order) => {
   };
 };
 
+export const listOrders = (artistId: string) => {
+  return async dispatch => {
+    dispatch(postOrderStarted());
+    const queryParams = `?orderBy="artistId"&equalTo="${artistId}"`;
+    const response = await fetch(`${config.firebase.databaseURL}/order.json${queryParams}`);
+    const data = await response.json();
+    let orders: OrderModel[];
+    if (!data) {
+      dispatch(postOrderEnded());
+      return;
+    }
+
+    orders = Object.keys(data).map(orderId => ({ id: orderId, ...data[orderId] }));
+
+    console.log('[OrderActions.tsx] listOrders(): orders: ', orders);
+    dispatch(storeOrders(orders));
+    dispatch(postOrderEnded());
+  };
+};
+
+export const storeOrders = (orders: OrderModel[]) => {
+  return {
+    type: STOREORDERS,
+    orders,
+  };
+};
+
+export const listOrdersStarted = () => {
+  return {
+    type: LISTORDERSSTARTED,
+  };
+};
+
+export const listOrdersEnded = () => {
+  return {
+    type: LISTORDERSENDED,
+  };
+};
 
 export const postOrderStarted = () => {
   return {
