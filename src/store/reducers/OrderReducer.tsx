d9d156eb9ce05a9727da555e2c430b1e551da53c
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-21 17:09:12 -0300

    reducer add new methods
    setCurrentOrder, delayOrder

--- src/store/reducers/OrderReducer.tsx
+++ src/store/reducers/OrderReducer.tsx
@@ -1,11 +1,21 @@
-import { STOREORDERS, POSTORDER, POSTORDERSTARTED, POSTORDERENDED, POSTORDERERROR } from '../actions/ActionTypes';
+import {
+  STOREORDERS,
+  POSTORDER,
+  POSTORDERSTARTED,
+  POSTORDERENDED,
+  POSTORDERERROR,
+  LISTORDERSSTARTED,
+  LISTORDERSENDED,
+  SETCURRENTORDER,
+  DELAYORDER,
+} from '../actions/ActionTypes';
 
 const INITIAL_STATE = {
   orders: null,
+  currentOrder: null,
   loading: false,
   error: null,
 };
-
 
 const storeOrders = (state, action) => {
   return {
@@ -35,11 +45,41 @@ const postOrderEnded = state => {
   };
 };
 
+const listOrdersStarted = state => {
+  return {
+    ...state,
+    loading: true,
+  };
+};
+
+const listOrdersEnded = state => {
+  return {
+    ...state,
+    loading: false,
+  };
+};
+
 const postOrderError = (state, action) => {
   return {
     ...state,
     loading: false,
     error: action.error,
+  };
+};
+
+const setCurrentOrder = (state, action) => {
+  return {
+    ...state,
+    currentOrder: action.order,
+  };
+};
+
+const delayOrder = (state, action) => {
+  const orders = state.orders.filter(order => order.id !== action.orderId);
+
+  return {
+    ...state,
+    orders,
   };
 };
 
@@ -55,6 +95,14 @@ const orderReducer = (state = INITIAL_STATE, action) => {
       return postOrderEnded(state);
     case POSTORDERERROR:
       return postOrderError(state, action);
+    case LISTORDERSSTARTED:
+      return listOrdersStarted(state);
+    case LISTORDERSENDED:
+      return listOrdersEnded(state);
+    case SETCURRENTORDER:
+      return setCurrentOrder(state, action);
+    case DELAYORDER:
+      return delayOrder(state, action);
     default:
       return state;
   }
