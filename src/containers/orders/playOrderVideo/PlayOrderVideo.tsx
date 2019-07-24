8323547f4a53169a8a1485bf7d5a57a89b2cf585
lmarqs <marques.analista@gmail.com>
2019-07-23 22:05:35 -0300

    create video player component

--- src/containers/orders/playOrderVideo/PlayOrderVideo.tsx
+++ src/containers/orders/playOrderVideo/PlayOrderVideo.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
+import { Video } from 'expo-av';
 import { OrderModel } from '@favid-inc/api';
 
 import * as actions from '../../../store/actions';
+
+import { VideoPlayer } from './videoPlayer';
 
 interface StoreState {
   order: OrderModel;
@@ -19,19 +22,45 @@ interface Props extends StoreState, StoreDispatch {
 
 class AbstractPlayOrderVideo extends Component<Props> {
   public render(): React.ReactNode {
-    return <Text>{JSON.stringify(this.props.order, null, 2)}</Text>;
+    return (
+      <View style={styles.container}>
+        <VideoPlayer
+          videoProps={{
+            shouldPlay: true,
+            resizeMode: Video.RESIZE_MODE_COVER,
+            source: {
+               uri: this.props.order.video,
+            },
+          }}
+          showControlsOnLoad={true}
+          showFullscreenButton={false}
+          isPortrait={true}
+        />
+      </View>
+    );
   }
 }
 
-const mapStateToProps = ({ order }) => ({
-  order: order.currentOrder,
-} as StoreState);
+const mapStateToProps = ({ order }) =>
+  ({
+    order: order.currentOrder,
+  } as StoreState);
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentOrder: (order: OrderModel) => dispatch(actions.setCurrentOrder(order)),
-} as StoreDispatch);
+const mapDispatchToProps = dispatch =>
+  ({
+    setCurrentOrder: (order: OrderModel) => dispatch(actions.setCurrentOrder(order)),
+  } as StoreDispatch);
 
 export const PlayOrderVideo = connect(
   mapStateToProps,
   mapDispatchToProps,
 )(AbstractPlayOrderVideo);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+});
