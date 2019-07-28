1022d7b3062bcabfdc00bec9ef4d59d3297ffddf
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-28 20:18:38 -0300

    upload video page changes

--- src/containers/orders/uploadOrderVideo/UploadOrderVideo.tsx
+++ src/containers/orders/uploadOrderVideo/UploadOrderVideo.tsx
@@ -1,7 +1,99 @@
 import React, { Component } from 'react';
+import { ActivityIndicator, Text, View, StyleSheet, Alert, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { OrderModel, OrderFlow, OrderStatus } from '@favid-inc/api';
+import * as actions from '../../../store/actions';
+import * as config from '@src/core/config';
 
-export class UploadOrderVideo extends Component {
+interface StoreState {
+  order: OrderModel;
+}
+
+interface StoreDispatch {
+  setCurrentOrder: (order: OrderModel) => void;
+}
+
+interface Props extends StoreState, StoreDispatch {
+  onDone: () => void;
+}
+
+interface State {
+  isUploading: boolean;
+}
+
+class AbstractUploadOrderVideo extends Component<Props, State> {
+  public componentDidMount() {
+    this.doUpload();
+  }
+
+  private async doUpload() {
+    this.setState({ isUploading: true });
+
+    try {
+      const data = new FormData();
+
+      data.append('video', {
+        type: 'video/mp4',
+        uri: this.props.order.video,
+      });
+
+      const response = await fetch(`${config.api.baseURL}/${OrderFlow.PLACE}/${this.props.order.id}`, {
+        method: 'PUT',
+        body: data,
+      });
+
+      const order = (await response.json()) as OrderModel;
+
+      this.props.setCurrentOrder(order);
+    } catch (e) {
+      Alert.alert('Erro', 'Não foi possível enviar o vídeo. Verifique sua conexão e tente novamente.');
+    }
+    this.setState({ isUploading: false });
+  }
+
   public render(): React.ReactNode {
-    return;
+    if (this.state.isUploading) {
+      return <SendingIndicator />;
+    }
+
+    if (this.props.order.status !== OrderStatus.OPENED) {
+      return <SuccessMessage onPress={this.props.onDone} />;
+    }
+
+    return <SendButton onPress={this.doUpload} />;
   }
 }
+
+const SendingIndicator = () => (
+  <View>
+    <ActivityIndicator size='large' color='#0000ff' />
+    <Text>Enviando vídeo</Text>
+  </View>
+);
+
+const SendButton = (props) => (
+  <View>
+    <Button title='Enviar Vídeo' {...props} />
+  </View>
+);
+
+
+const SuccessMessage = (props) => (
+  <View>
+    <Text>Vídeo enviado com sucesso!</Text>
+    <Button title='Voltar' {...props} />
+  </View>
+);
+
+const mapStateToProps = ({ order }) =>
+  ({
+    order: order.currentOrder,
+  } as StoreState);
+
+const mapDispatchToProps = dispatch =>
+  ({ setCurrentOrder: (order: OrderModel) => dispatch(actions.setCurrentOrder(order)) } as StoreDispatch);
+
+export const UploadOrderVideo = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AbstractUploadOrderVideo);
--- src/containers/orders/uploadOrderVideo/UploadOrderVideoContainer.tsx
+++ src/containers/orders/uploadOrderVideo/UploadOrderVideoContainer.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { NavigationScreenProps } from 'react-navigation';
+import { UploadOrderVideo } from './UploadOrderVideo';
+import { StyleSheet, View } from 'react-native';
 
 export class UploadOrderVideoContainer extends Component<NavigationScreenProps> {
+  private handleOnDone = () => {
+    this.props.navigation.goBack('SelectOrder');
+  };
+
   public render(): React.ReactNode {
-    return;
+    return (
+      <View style={styles.container}>
+        <UploadOrderVideo onUpload={this.handleOnDone} />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
