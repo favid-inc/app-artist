36dc7ac3025a8e8b982a369b82ea2919995ee2d3
lmarqs <marques.analista@gmail.com>
2019-08-04 23:28:24 -0300

    do not play video in background

--- src/containers/orders/playOrderVideo/PlayOrderVideoContainer.tsx
+++ src/containers/orders/playOrderVideo/PlayOrderVideoContainer.tsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { NavigationScreenProps } from 'react-navigation';
+import { NavigationScreenProps, NavigationEventSubscription } from 'react-navigation';
+import { View } from 'react-native';
 import { PlayOrderVideo } from './PlayOrderVideo';
 
-export class PlayOrderVideoContainer extends Component<NavigationScreenProps> {
+interface State {
+  didFocusSubscription?: NavigationEventSubscription;
+  didBlurSubscription?: NavigationEventSubscription;
+  isFocused?: boolean;
+}
+
+export class PlayOrderVideoContainer extends Component<NavigationScreenProps, State> {
+  public state = {
+    didFocusSubscription: null,
+    didBlurSubscription: null,
+    isFocused: false,
+  };
+
   private handleOnRedo = () => {
     this.props.navigation.goBack();
   };
@@ -11,7 +24,24 @@ export class PlayOrderVideoContainer extends Component<NavigationScreenProps> {
     this.props.navigation.push('UploadOrderVideo');
   };
 
+  public componentDidMount = () => {
+    const { navigation } = this.props;
+    this.setState({
+      didFocusSubscription: navigation.addListener('didFocus', () => this.setState({ isFocused: true })),
+      didBlurSubscription: navigation.addListener('didBlur', () => this.setState({ isFocused: false })),
+    });
+  };
+
+  public componentWillUnmount = () => {
+    const { didFocusSubscription, didBlurSubscription } = this.state;
+    didFocusSubscription && didFocusSubscription.remove();
+    didBlurSubscription && didBlurSubscription.remove();
+  };
+
   public render() {
+    if (!this.state.isFocused) {
+      return <View />;
+    }
     return <PlayOrderVideo onRedo={this.handleOnRedo} onUpload={this.handleOnUpload} />;
   }
 }
