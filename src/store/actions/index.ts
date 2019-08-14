9fa93fbf1cae39af3649683514a810f3ce90856a
lmarqs <marques.analista@gmail.com>
2019-08-14 20:04:37 -0300

    kill redux

--- src/store/actions/index.ts
+++
@@ -1,2 +0,0 @@
-export { auth, loadAuthState, signOut, verifySession } from './AuthActions';
-export { setArtist, getArtist, removeArtist, listArtists, putArtist, loadArtist, artistError } from './ArtistActions';
