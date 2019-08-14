3028fda75fc3abd7848a349e81ef14203981ad73
lmarqs <marques.analista@gmail.com>
2019-08-14 11:56:30 -0300

    create apiClient

--- src/core/utils/apiClient.ts
+++ src/core/utils/apiClient.ts
@@ -3,11 +3,11 @@ import * as firebase from 'firebase';
 
 import * as config from '@src/core/config';
 
-export const axios = axiosRaw.create({
+export const apiClient = axiosRaw.create({
   baseURL: config.api.baseURL,
 });
 
 firebase.auth().onIdTokenChanged(async auth => {
   const idToken = await auth.getIdToken();
-  axios.defaults.headers.commons.Authorization = `Bearer ${idToken}`;
+  apiClient.defaults.headers.commons.Authorization = `Bearer ${idToken}`;
 });
