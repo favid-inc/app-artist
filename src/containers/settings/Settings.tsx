dd50a60ffdb9cfaed884a514c4cad166de6630f5
lmarqs <marques.analista@gmail.com>
2019-08-25 11:03:15 -0300

    add complaince buttons

--- src/containers/settings/Settings.tsx
+++ src/containers/settings/Settings.tsx
@@ -1,8 +1,8 @@
 import { ThemedComponentProps, ThemeType, withStyles } from '@kitten/theme';
 import { Button, Text } from '@kitten/ui';
 import React from 'react';
-import { View } from 'react-native';
-import { BookFill, LogOutIconFill as logOutIconFill, PersonIconFill } from '@src/assets/icons';
+import { Linking, View } from 'react-native';
+import { BookFill, FileTextIconFill, LogOutIconFill, PersonIconFill, MenuIconMessaging } from '@src/assets/icons';
 import { AuthContext } from '@src/core/auth';
 // import { loadProfile } from './account/loadProfile';
 
@@ -33,6 +33,9 @@ class SettingsComponent extends React.Component<SettingsComponentProps, State> {
         />
         <NavigateToAccountButton themedStyle={themedStyle} onNavigate={this.handleNavigateToAccount} />
         <SigOutButton themedStyle={themedStyle} />
+        <NeedHelpButton themedStyle={themedStyle} />
+        <PoliciesButton themedStyle={themedStyle} />
+        <TermsButton themedStyle={themedStyle} />
       </View>
     );
   }
@@ -96,8 +99,53 @@ const SigOutButton = ({ themedStyle }) => {
   const handleSignOutClick = React.useCallback(() => context.signOut(), [context]);
 
   return (
-    <Button status='danger' style={themedStyle.Button} onPress={handleSignOutClick} icon={logOutIconFill} size='giant'>
+    <Button status='danger' style={themedStyle.Button} onPress={handleSignOutClick} icon={LogOutIconFill} size='giant'>
       Deslogar
+    </Button>
+  );
+};
+
+const NeedHelpButton = ({ themedStyle }) => {
+  const handleClick = React.useCallback(
+    () => Linking.openURL('mailto:suporte.favid@gmail.com?subject=Preciso de Ajuda com o aplicativo&'),
+    [],
+  );
+
+  return (
+    <Button status='primary' style={themedStyle.Button} onPress={handleClick} icon={MenuIconMessaging} size='giant'>
+      Preciso de Ajuda
+    </Button>
+  );
+};
+
+const PoliciesButton = ({ themedStyle }) => {
+  const handleClick = React.useCallback(
+    () =>
+      Linking.openURL(
+        'https://onyx-harmony-239219.firebaseapp.com/terms-and-policies/Poli%CC%81tica%20de%20Seguranc%CC%A7a%20e%20Privacidade%20-%20FAVID%20-%2011.04.2019.pdf',
+      ),
+    [],
+  );
+
+  return (
+    <Button status='primary' style={themedStyle.Button} onPress={handleClick} icon={FileTextIconFill} size='giant'>
+      Politica de Segurança/Privacide
+    </Button>
+  );
+};
+
+const TermsButton = ({ themedStyle }) => {
+  const handleClick = React.useCallback(
+    () =>
+      Linking.openURL(
+        'https://onyx-harmony-239219.firebaseapp.com/terms-and-policies/Termos%20de%20Uso%20do%20Site%20ou%20Aplicativo%20-%20FAVID%20-%2011.04.2019.pdf',
+      ),
+    [],
+  );
+
+  return (
+    <Button status='primary' style={themedStyle.Button} onPress={handleClick} icon={FileTextIconFill} size='giant'>
+      Termos de Uso
     </Button>
   );
 };
