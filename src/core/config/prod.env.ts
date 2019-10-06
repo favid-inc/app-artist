0fe87933ef3e9c48c0ee3ca61657f6bf8e3f70be
lmarqs <marques.analista@gmail.com>
2019-10-06 13:03:21 -0300

    adjust google prod keys

--- src/core/config/prod.env.ts
+++ src/core/config/prod.env.ts
@@ -14,8 +14,7 @@ export const api = {
 
 export const auth = {
   google: {
-    behavior: 'web',
-    clientId: '983263249595-2tclp7pj3b23h05l08v8v7af6mgbud1s.apps.googleusercontent.com',
+    clientId: '983263249595-u1brakoaa7qltqt6akrl44mmtu9ab0pa.apps.googleusercontent.com',
     issuer: 'https://accounts.google.com',
     scopes: ['profile', 'email'],
   },
