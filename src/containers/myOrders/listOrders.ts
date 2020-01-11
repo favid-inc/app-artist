c25e87b08bb7e7921216120cedfd279a502408dd
lmarqs <marques.analista@gmail.com>
2020-01-11 19:53:58 -0300

    add order status to listOrders

--- src/containers/myOrders/listOrders.ts
+++ src/containers/myOrders/listOrders.ts
@@ -1,10 +1,14 @@
 import { ListOrders as Action } from '@favid-inc/api/lib/app-artist';
 import { apiClient } from '@src/core/utils/apiClient';
+import { OrderStatus } from '@favid-inc/api';
 
 export async function listOrders(): Promise<Action['Response']> {
   const request: Action['Request'] = {
     url: '/ListOrders',
     method: 'GET',
+    params: {
+      orderStatus: OrderStatus.FULFILLED,
+    }
   };
 
   const response = await apiClient.request<Action['Response']>(request);
