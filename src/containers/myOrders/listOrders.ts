2f1a3513d1197a73c85a2fa2239d4bc165dbbbda
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2020-01-11 23:48:44 +0100

    add request myorders

---
+++ src/containers/myOrders/listOrders.ts
@@ -0,0 +1,13 @@
+import { ListOrders as Action } from '@favid-inc/api/lib/app-artist';
+import { apiClient } from '@src/core/utils/apiClient';
+
+export async function listOrders(): Promise<Action['Response']> {
+  const request: Action['Request'] = {
+    url: '/ListOrders',
+    method: 'GET',
+  };
+
+  const response = await apiClient.request<Action['Response']>(request);
+
+  return response.data;
+}
